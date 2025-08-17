71884795d7e1eabdae2f7c5645a38ddfc51d7bfc
Nilav <nilavtalukdar06@gmail.com>
2025-08-17 11:03:45 +0530

    added rate limiting to the api

--- src/app/api/send-message/route.ts
+++ src/app/api/send-message/route.ts
@@ -1,14 +1,42 @@
 import connectToDb from "@/db/mongodb";
 import Message from "@/models/message.model";
 import { NextRequest, NextResponse } from "next/server";
+import arcjet, { tokenBucket } from "@arcjet/next";
 import { z } from "zod";
 
 const schema = z.object({
   message: z.string().min(2, { message: "message is too short" }),
 });
 
+const aj = arcjet({
+  key: process.env.ARCJET_KEY!,
+  rules: [
+    tokenBucket({
+      mode: "LIVE",
+      characteristics: ["ip.src"],
+      refillRate: 5,
+      interval: 5,
+      capacity: 15,
+    }),
+  ],
+});
+
 export async function POST(request: NextRequest) {
   try {
+    const decision = await aj.protect(request, { requested: 5 });
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        return NextResponse.json(
+          { error: "Too Many Requests", reason: decision.reason },
+          { status: 429 }
+        );
+      }
+      return NextResponse.json(
+        { error: "Forbidden", reason: decision.reason },
+        { status: 403 }
+      );
+    }
+
     const body = await request.json();
     const parsedBody = schema.safeParse(body);
     if (!parsedBody.success) {
