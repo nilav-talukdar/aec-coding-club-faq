772d5adcfa518d8f10f9a91025b15f76bf6c5c96
Nilav <nilavtalukdar06@gmail.com>
2025-08-17 13:10:03 +0530

    added api routes for getting messages

---
+++ src/app/api/message/get-messages/route.ts
@@ -0,0 +1,26 @@
+import connectToDb from "@/db/mongodb";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+import Message from "@/models/message.model";
+
+export async function GET() {
+  try {
+    const session = await getServerSession(authOptions);
+    if (!session) {
+      return NextResponse.json(
+        { error: "you are not an admin" },
+        { status: 401 }
+      );
+    }
+    await connectToDb();
+    const result = await Message.find({});
+    return NextResponse.json(result, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "failed to get messages" },
+      { status: 500 }
+    );
+  }
+}
