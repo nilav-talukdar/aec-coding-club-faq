3d27285dfbe6c0e99a6c493f19f69f758c95ad89
Nilav <nilavtalukdar06@gmail.com>
2025-08-17 09:30:33 +0530

    improved ui of the layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,4 +1,6 @@
+import { Button } from "@/components/ui/button";
 import { authOptions } from "@/lib/auth";
+import { Lock } from "lucide-react";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 
@@ -9,5 +11,22 @@ export default async function Layout({
   if (session) {
     redirect("/admin");
   }
-  return <div>{children}</div>;
+  return (
+    <div className="max-w-md p-4 mx-auto my-24">
+      <div className="flex flex-col justify-center items-center gap-2">
+        <div className="flex justify-center items-center gap-4">
+          <Button className="pointer-events-none" size="sm" variant="secondary">
+            <Lock />
+          </Button>
+          <p className="text-2xl font-medium text-neutral-700 mb-1">
+            Admin Panel
+          </p>
+        </div>
+        <p className="text-sm text-center font-light text-neutral-500">
+          Enter the admin credentials to continue
+        </p>
+      </div>
+      <div>{children}</div>
+    </div>
+  );
 }
