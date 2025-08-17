0a611aeb07bc714c42504c67d862a39942c7c3ed
Nilav <nilavtalukdar06@gmail.com>
2025-08-17 13:16:36 +0530

    added the api for deleting a message

---
+++ src/app/api/message/delete-message/route.ts
@@ -0,0 +1,42 @@
+import connectToDb from "@/db/mongodb";
+import { authOptions } from "@/lib/auth";
+import Message from "@/models/message.model";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+const query = z.object({
+  messageId: z.string().min(2, { message: "messageId is too short" }),
+});
+
+export async function DELETE(request: NextRequest) {
+  try {
+    const session = await getServerSession(authOptions);
+    if (!session) {
+      return NextResponse.json(
+        { error: "you are not an admin" },
+        { status: 401 }
+      );
+    }
+    await connectToDb();
+    const messageId = request.nextUrl.searchParams.get("messageId");
+    const parsedQuery = query.safeParse({ messageId });
+    if (!parsedQuery.success) {
+      return NextResponse.json(
+        { error: parsedQuery.error.flatten().fieldErrors },
+        { status: 400 }
+      );
+    }
+    await Message.findByIdAndDelete(parsedQuery.data);
+    return NextResponse.json(
+      { success: true, message: "message deleted successfully" },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "failed to delete message" },
+      { status: 500 }
+    );
+  }
+}
