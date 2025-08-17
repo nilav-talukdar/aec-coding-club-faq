3aec039749e81f72b78cf0321c17a9e27c2603e5
Nilav <nilavtalukdar06@gmail.com>
2025-08-17 09:39:06 +0530

    defined form schema

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,6 +1,33 @@
 "use client";
 
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+const formSchema = z.object({
+  username: z
+    .string()
+    .min(2, { message: "Username is too short" })
+    .max(50, { message: "Username is too long" }),
+  password: z
+    .string()
+    .min(8, { message: "Password is too short" })
+    .max(50, { message: "Password is too long" }),
+});
+
 export default function Login() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
   return (
     <section>
       <h1>Login</h1>
