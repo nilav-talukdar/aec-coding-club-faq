8ab405a665e6e4dc9cb5be1665b38ddb6781e0ba
Nilav <nilavtalukdar06@gmail.com>
2025-08-17 13:40:32 +0530

    fetching data dynamically

--- src/app/(routes)/admin/page.tsx
+++ src/app/(routes)/admin/page.tsx
@@ -1,19 +1,42 @@
+"use client";
 import LogoutButton from "@/components/auth/logout-button";
 import Card from "@/components/shared/card";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { Loader } from "lucide-react";
 
 export default function AdminPage() {
+  const query = useQuery({
+    queryKey: ["message"],
+    queryFn: async () => {
+      const result = await axios.get("/api/message/get-messages");
+      return result.data;
+    },
+  });
+
   return (
     <section>
       <h1>Admin</h1>
       <LogoutButton />
       <div className="my-6">
         <p className="text-2xl font-medium text-neutral-600">FAQs</p>
-        <div className="my-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-        </div>
+        {query.isLoading ? (
+          <div className="my-6">
+            <Loader className="text-blue-500 animate-spin" size={24} />
+          </div>
+        ) : (
+          <div className="my-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+            {(query.data ?? []).map(
+              (item: { _id: string; message: string }, index: number) => (
+                <Card
+                  key={item._id}
+                  message={item.message}
+                  serial={index + 1}
+                />
+              )
+            )}
+          </div>
+        )}
       </div>
     </section>
   );
--- src/components/shared/card.tsx
+++ src/components/shared/card.tsx
@@ -1,14 +1,19 @@
 import { Trash } from "lucide-react";
 
-export default function Card() {
+interface CardProps {
+  message: string;
+  serial: number;
+}
+
+export default function Card({ message, serial }: CardProps) {
   return (
     <div className="p-4 text-base sm:text-lg rounded-lg shadow-sm border">
       <div className="w-full flex justify-between items-center text-sm font-light text-neutral-400">
-        <p>Question 1</p>
-        <Trash size={16} className="text-red-500" />
+        <p>Question: {serial}</p>
+        <Trash size={16} className="text-red-500 cursor-pointer" />
       </div>
       <div className="text-lg text-neutral-600 my-2">
-        <p>is AI/ML really that difficult</p>
+        <p>{message}</p>
       </div>
     </div>
   );
