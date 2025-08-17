c02c51a20928b2cf8e635f225b1764318277fa5f
Nilav <nilavtalukdar06@gmail.com>
2025-08-17 09:52:11 +0530

    added query client

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
+import QueryProvider from "@/components/ui/context/query-client";
 
 const nunito = Nunito({
   subsets: ["latin"],
@@ -17,11 +18,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${nunito.className} antialiased`}>
-        <main>{children}</main>
-        <Toaster />
-      </body>
-    </html>
+    <QueryProvider>
+      <html lang="en">
+        <body className={`${nunito.className} antialiased`}>
+          <main>{children}</main>
+          <Toaster />
+        </body>
+      </html>
+    </QueryProvider>
   );
 }
